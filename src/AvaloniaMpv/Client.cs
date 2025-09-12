3b6b867810da36561110458ae7440c4f7e9f81ee
Noble <nobleeugene2005@gmail.com>
2025-09-12 08:25:24 +0100

    initial commit, managed to get mpv working in an opengl context provided by opentk

---
+++ src/AvaloniaMpv/Client.cs
@@ -0,0 +1,252 @@
+﻿using System.Runtime.InteropServices;
+namespace AvaloniaMpv;
+public static class LibMpv
+{
+    private const string LibName = "mpv"; // TODO: handle platform differences
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void mp_clients_init(nint mpctx);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int mpv_set_option_string(
+       nint ctx,
+       [MarshalAs(UnmanagedType.LPStr)] string name,
+       [MarshalAs(UnmanagedType.LPStr)] string data
+   );
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int mpv_initialize(nint ctx);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern nint mpv_create();
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void mp_clients_destroy(nint mpctx);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void mp_shutdown_clients(nint mpctx);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static extern bool mp_is_shutting_down(nint mpctx);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static extern bool mp_clients_all_initialized(nint mpctx);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static extern bool mp_client_id_exists(nint mpctx, long id);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void mp_client_broadcast_event(nint mpctx, int @event, nint data);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int mp_client_send_event(
+        nint mpctx,
+        [MarshalAs(UnmanagedType.LPStr)] string client_name,
+        ulong reply_userdata,
+        int @event,
+        nint data);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int mp_client_send_event_dup(
+        nint mpctx,
+        [MarshalAs(UnmanagedType.LPStr)] string client_name,
+        int @event,
+        nint data);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void mp_client_property_change(
+        nint mpctx,
+        [MarshalAs(UnmanagedType.LPStr)] string name);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void mp_client_send_property_changes(nint mpctx);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern nint mp_new_client(nint clients, [MarshalAs(UnmanagedType.LPStr)] string name);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void mp_client_set_weak(nint ctx);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern nint mp_client_get_log(nint ctx);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern nint mp_client_get_global(nint ctx);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void mp_client_broadcast_event_external(nint api, int @event, nint data);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern nint node_get_alloc(nint node);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static extern bool mp_set_main_render_context(nint client_api, nint ctx, bool active);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern nint mp_client_api_acquire_render_context(nint ca);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void kill_video_async(nint client_api);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate int mpv_stream_cb_open_ro_fn(
+        nint user_data,
+        [MarshalAs(UnmanagedType.LPStr)] string uri,
+        out nint stream);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static extern bool mp_streamcb_lookup(
+        nint g,
+        [MarshalAs(UnmanagedType.LPStr)] string protocol,
+        out nint out_user_data,
+        out mpv_stream_cb_open_ro_fn out_fn);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int mpv_request_log_messages(nint ctx, [MarshalAs(UnmanagedType.LPStr)] string min_level);
+
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int mpv_render_context_create(
+            out nint res,
+            nint mpv,
+            nint @params
+        );
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int mpv_command_async(nint ctx, UInt64 reply_userdata, nint args);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int mpv_command(nint ctx, nint args);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void mpv_set_wakeup_callback(nint ctx, MpvWakeupCallback cb, nint d);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern nint mpv_wait_event(nint ctx, double timeout);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void mpv_render_context_set_update_callback(nint ctx, MpvRenderUpdateFn callback, nint callback_ctx);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern UInt64 mpv_render_context_update(nint ctx);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern UInt64 mpv_render_context_render(nint ctx , nint @params);
+}
+
+
+public enum mpv_render_param_type
+{
+    MPV_RENDER_PARAM_INVALID = 0,
+    MPV_RENDER_PARAM_API_TYPE = 1,
+    MPV_RENDER_PARAM_OPENGL_INIT_PARAMS = 2,
+    MPV_RENDER_PARAM_OPENGL_FBO = 3,
+    MPV_RENDER_PARAM_FLIP_Y = 4,
+    MPV_RENDER_PARAM_DEPTH = 5,
+    MPV_RENDER_PARAM_ICC_PROFILE = 6,
+    MPV_RENDER_PARAM_AMBIENT_LIGHT = 7,
+    MPV_RENDER_PARAM_X11_DISPLAY = 8,
+    MPV_RENDER_PARAM_WL_DISPLAY = 9,
+    MPV_RENDER_PARAM_ADVANCED_CONTROL = 10,
+    MPV_RENDER_PARAM_NEXT_FRAME_INFO = 11,
+    MPV_RENDER_PARAM_BLOCK_FOR_TARGET_TIME = 12,
+    MPV_RENDER_PARAM_SKIP_RENDERING = 13,
+    MPV_RENDER_PARAM_DRM_DISPLAY = 14,
+    MPV_RENDER_PARAM_DRM_DRAW_SURFACE_SIZE = 15,
+    MPV_RENDER_PARAM_DRM_DISPLAY_V2 = 16,
+    MPV_RENDER_PARAM_SW_SIZE = 17,
+    MPV_RENDER_PARAM_SW_FORMAT = 18,
+    MPV_RENDER_PARAM_SW_STRIDE = 19,
+    MPV_RENDER_PARAM_SW_POINTER = 20,
+};
+[StructLayout(LayoutKind.Sequential)]
+public struct MpvRenderParam
+{
+    public mpv_render_param_type type;
+    public nint data;
+}
+
+[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+public delegate nint MpvOpenglGetProcAddressCallback(
+    nint ctx,
+    [MarshalAs(UnmanagedType.LPStr)] string name
+);
+[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+public delegate void MpvRenderUpdateFn(
+    nint cb_ctx
+);
+[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+public delegate void MpvWakeupCallback(nint data);
+
+[StructLayout(LayoutKind.Sequential)]
+public struct MpvOpenglInitParams
+{
+    public MpvOpenglGetProcAddressCallback get_proc_address;
+    public nint get_proc_address_ctx;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct MpvEvent
+{
+    public MpvEventId event_id;
+    public int error;
+    public UInt64 reply_userdata;
+    public nint data;
+};
+
+[StructLayout(LayoutKind.Sequential)]
+public struct MpvOpenGLFramebuffer
+{
+    public int fbo;
+    public int width;
+    public int height;
+    public int internal_format;
+}
+
+public enum MpvEventId
+{
+    MPV_EVENT_NONE = 0,
+    MPV_EVENT_SHUTDOWN = 1,
+    MPV_EVENT_LOG_MESSAGE = 2,
+    MPV_EVENT_GET_PROPERTY_REPLY = 3,
+    MPV_EVENT_SET_PROPERTY_REPLY = 4,
+    MPV_EVENT_COMMAND_REPLY = 5,
+    MPV_EVENT_START_FILE = 6,
+    MPV_EVENT_END_FILE = 7,
+    MPV_EVENT_FILE_LOADED = 8,
+    MPV_EVENT_IDLE = 11,
+    MPV_EVENT_TICK = 14,
+    MPV_EVENT_CLIENT_MESSAGE = 16,
+    MPV_EVENT_VIDEO_RECONFIG = 17,
+    MPV_EVENT_AUDIO_RECONFIG = 18,
+    MPV_EVENT_SEEK = 20,
+    MPV_EVENT_PLAYBACK_RESTART = 21,
+    MPV_EVENT_PROPERTY_CHANGE = 22,
+    MPV_EVENT_QUEUE_OVERFLOW = 24,
+    MPV_EVENT_HOOK = 25,
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct MpvEventLogMessage
+{
+    public nint prefix;
+    public nint level;
+    public nint text;
+    public MpvLogLevel log_level;
+};
+public enum MpvLogLevel
+{
+    MPV_LOG_LEVEL_NONE = 0,    /// "no"    - disable absolutely all messages
+    MPV_LOG_LEVEL_FATAL = 10,   /// "fatal" - critical/aborting errors
+    MPV_LOG_LEVEL_ERROR = 20,   /// "error" - simple errors
+    MPV_LOG_LEVEL_WARN = 30,   /// "warn"  - possible problems
+    MPV_LOG_LEVEL_INFO = 40,   /// "info"  - informational message
+    MPV_LOG_LEVEL_V = 50,   /// "v"     - noisy informational message
+    MPV_LOG_LEVEL_DEBUG = 60,   /// "debug" - very noisy technical information
+    MPV_LOG_LEVEL_TRACE = 70,   /// "trace" - extremely noisy
+};
---
+++ src/Sandbox/Program.cs
@@ -0,0 +1,194 @@
+﻿namespace Program;
+using OpenTK.Graphics.OpenGL4;
+using AvaloniaMpv;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System.Runtime.InteropServices;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System.Collections.Concurrent;
+
+public class Window : GameWindow
+{
+    private ConcurrentQueue<Action> _eventQueue = new();
+    private nint _mpvContext = nint.Zero;
+    private nint _mpvRenderContext = nint.Zero;
+    private bool _redraw = false;
+    public Window(int width, int height, string title)
+        : base(GameWindowSettings.Default, new NativeWindowSettings()
+        {
+            ClientSize = (width, height),
+            Title = title
+        })
+    { }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+        GL.ClearColor(0.4f, 0.2f, 0.3f, 1.0f); // dark blue-ish background
+
+        var mpv = LibMpv.mpv_create();
+        _mpvContext = mpv;
+        if (mpv == nint.Zero)
+        {
+            Console.WriteLine("Some crazy error");
+        }
+        LibMpv.mpv_set_option_string(mpv, "vo", "libmpv");
+        if (LibMpv.mpv_initialize(mpv) < 0)
+        {
+            Console.WriteLine("MPV failed to init");
+        }
+        LibMpv.mpv_request_log_messages(mpv, "debug");
+        MpvOpenglGetProcAddressCallback procCallback = Window.GetProcAddress;
+        var initParams = new MpvOpenglInitParams
+        {
+            get_proc_address = procCallback,
+            get_proc_address_ctx = nint.Zero,
+        };
+        nint initParamsPtr = Marshal.AllocHGlobal(Marshal.SizeOf<MpvOpenglInitParams>());
+        Marshal.StructureToPtr(initParams, initParamsPtr, false);
+        var paramApiType = Marshal.StringToHGlobalAnsi("opengl");
+
+        var enableAdvancedControl = 1;
+        var enableAdvancedControlPtr = Marshal.AllocHGlobal(Marshal.SizeOf<int>());
+        Marshal.WriteInt32(enableAdvancedControlPtr, enableAdvancedControl);
+        MpvRenderParam[] renderParams = {
+            new(){
+                type = mpv_render_param_type.MPV_RENDER_PARAM_API_TYPE, data = paramApiType,
+            },
+            new() {
+                type = mpv_render_param_type.MPV_RENDER_PARAM_OPENGL_INIT_PARAMS , data = initParamsPtr
+            },
+            new() {
+                type = mpv_render_param_type.MPV_RENDER_PARAM_ADVANCED_CONTROL ,
+                 data = enableAdvancedControlPtr
+            },
+            new()
+        };
+        nint mpv_gl;
+        GCHandle handle = GCHandle.Alloc(renderParams, GCHandleType.Pinned);
+        nint pParams = handle.AddrOfPinnedObject();
+
+        int status = LibMpv.mpv_render_context_create(
+            out mpv_gl,
+            mpv,
+            pParams
+        );
+        _mpvRenderContext = mpv_gl;
+        handle.Free();
+        LibMpv.mpv_set_wakeup_callback(mpv, MpvEvent, nint.Zero);
+        //render callback;
+        LibMpv.mpv_render_context_set_update_callback(mpv_gl, MpvRenderUpdate, nint.Zero);
+        string[] command = { "loadfile", "/home/noble/Videos/Superman.2025.1080p.WebDl.English.Msubs.MoviesMod.cafe.mkv" };
+        nint[] argPtrs = new nint[command.Length + 1];
+        for (int i = 0; i < command.Length; i++)
+        {
+            argPtrs[i] = Marshal.StringToHGlobalAnsi(command[i]);
+        }
+        argPtrs[command.Length] = nint.Zero; // null terminator
+
+        nint argsPtr = Marshal.AllocHGlobal(nint.Size * argPtrs.Length);
+        Marshal.Copy(argPtrs, 0, argsPtr, argPtrs.Length);
+        int result = LibMpv.mpv_command_async(mpv, 0, argsPtr);
+        for (int i = 0; i < command.Length; i++)
+        {
+            if (argPtrs[i] != nint.Zero)
+                Marshal.FreeHGlobal(argPtrs[i]);
+        }
+        Marshal.FreeHGlobal(argsPtr);
+        Marshal.FreeHGlobal(paramApiType);
+        Marshal.DestroyStructure<MpvOpenglInitParams>(initParamsPtr);
+        Marshal.FreeHGlobal(enableAdvancedControlPtr);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+        // MakeCurrent();
+        GL.Clear(ClearBufferMask.ColorBufferBit); // clear screen
+        while (_eventQueue.TryDequeue(out var action))
+        {
+            action();
+        }
+
+        if (_redraw)
+        {
+            var size = (System.Drawing.Size)this.Size;
+            var w = size.Width;
+            var h = size.Height;
+            var flip_y = GCHandle.Alloc(1, GCHandleType.Pinned);
+            MpvOpenGLFramebuffer framebuffer = new()
+            {
+                fbo = 0,
+                width = w,
+                height = h,
+            };
+            var framebufferPtr = Marshal.AllocHGlobal(Marshal.SizeOf<MpvOpenGLFramebuffer>());
+            Marshal.StructureToPtr(framebuffer, framebufferPtr, false);
+            MpvRenderParam[] param = {
+              new() {
+                type = mpv_render_param_type.MPV_RENDER_PARAM_OPENGL_FBO,
+                data = framebufferPtr,
+              },
+                new() {
+                    type = mpv_render_param_type.MPV_RENDER_PARAM_FLIP_Y,
+                    data = flip_y.AddrOfPinnedObject(),
+                },
+                new()
+            };
+            var handle = GCHandle.Alloc(param, GCHandleType.Pinned);
+            var paramsP = handle.AddrOfPinnedObject();
+            LibMpv.mpv_render_context_render(_mpvRenderContext, paramsP);
+            handle.Free();
+            Marshal.FreeHGlobal(framebufferPtr);
+            _redraw = true;
+        }
+        SwapBuffers(); // show what we drew
+    }
+
+    public static nint GetProcAddress(nint fn_ctx, [MarshalAs(UnmanagedType.LPStr)] string name)
+    {
+        return GLFW.GetProcAddress(name);
+    }
+
+    private void MpvRenderUpdate(nint data)
+    {
+        _eventQueue.Enqueue(() =>
+        {
+            var flags = LibMpv.mpv_render_context_update(_mpvRenderContext);
+            if ((flags & (1 << 0)) != 0)
+            {
+                _redraw = true;
+            }
+        });
+    }
+    private void MpvEvent(nint data)
+    {
+        //i can probably do this safely on any thread
+        while (true)
+        {
+            nint evPtr = LibMpv.mpv_wait_event(_mpvContext, 0.0);
+            if (evPtr == nint.Zero) break;
+            MpvEvent ev = Marshal.PtrToStructure<MpvEvent>(evPtr);
+
+            if (ev.event_id == MpvEventId.MPV_EVENT_NONE) break;
+            if (ev.event_id == MpvEventId.MPV_EVENT_LOG_MESSAGE)
+            {
+                nint msgPtr = ev.data;
+                var msg = Marshal.PtrToStructure<MpvEventLogMessage>(msgPtr);
+                var text = Marshal.PtrToStringAnsi(msg.text);
+                if (text is null) break;
+                if (text.Contains("DR image"))
+                    Console.WriteLine($"Recieved Log Message {text}");
+            }
+        }
+    }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        using var w = new Window(800, 600, "LearnOpenGL with OpenTK");
+        w.Run();
+    }
+}
